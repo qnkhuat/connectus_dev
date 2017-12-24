6d277ca24cb0b3b2cb9175f1e57086eeb226630a
Ho Ngoc Trung <trung0194@gmail.com>
2017-12-24 16:51:42 +0700

    changed login button

--- resources/views/front/components/login.blade.php
+++ resources/views/front/components/login.blade.php
@@ -1,50 +1,5 @@
 <div class="login-tab">
-
   <div class="login-facebook login-tab-button">
-    <a href="" class="df"><i class="fa fa-facebook" aria-hidden="true"></i>Đăng nhập bằng Facebook</a>
+    <a href="/redirect/facebook" class="df"><i class="fa fa-facebook" aria-hidden="true"></i>Đăng nhập bằng Facebook</a>
   </div>
-
-  <div class="signup-button login-tab-button">
-    <p class="df"><i class="fa fa-rocket" aria-hidden="true"></i> Đăng ký tài khoản</p>
-  </div>
-
-  <div class="signin-button login-tab-button">
-    <p class="df"><i class="fa fa-sign-in" aria-hidden="true"></i> Đăng nhập</p>
-  </div>
-
-  <div class="separate df">
-    <hr>
-    <p>Hoặc</p>
-    <hr>
-  </div>
-
-  <div class="user-infomations">
-
-    <div class="email input-user-infomations">
-      <input type="email" placeholder="E-mail">
-    </div>
-
-    <div class="password input-user-infomations">
-      <input type="password" placeholder="Mật khẩu">
-    </div>
-
-    <div class="username input-user-infomations">
-      <input type="text" placeholder="Tên hiện thị">
-    </div>
-
-    <hr>
-
-    <div class="login-submit submit">
-      <input type="submit" class="button" Value="Đăng nhập">
-    </div>
-
-    <div class="signup-submit submit">
-      <input type="submit" class="button" Value="Đăng ký">
-    </div>
-
-    <div class="forget-password">
-      <a href="">Quên mật khẩu</a>
-    </div>
-    @yield('addtion_login')
-  </div><!-- .user-infomations -->
 </div><!-- .login-tab -->
