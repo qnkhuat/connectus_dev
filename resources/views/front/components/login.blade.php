5d1982a9aaa3449d2a042601aa7d1e7e3efe01bd
Ho Ngoc Trung <trung0194@gmail.com>
2017-12-29 10:43:27 +0700

    fix login facebook

--- resources/views/front/components/login.blade.php
+++ resources/views/front/components/login.blade.php
@@ -1,7 +1,7 @@
 <div class="login-tab">
 
   <div class="login-facebook login-tab-button">
-    <a href="" class="df"><i class="fa fa-facebook" aria-hidden="true"></i>Đăng nhập bằng Facebook</a>
+    <a href="/redirect/facebook" class="df"><i class="fa fa-facebook" aria-hidden="true"></i>Đăng nhập bằng Facebook</a>
   </div>
 
   <div class="signup-button login-tab-button">
