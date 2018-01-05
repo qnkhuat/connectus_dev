d3a5dea3d5f5200d0d62ad7e6661e0b44e7cf5fd
Ngoc <khuatquangngoc98@gmail.com>
2018-01-05 18:49:45 +0700

    add offline roboto font

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -11,8 +11,7 @@
     <script src="/backend/assets/js/modernizr.min.js"></script>
 
     <!------------FONTS -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,500,400,700" rel="stylesheet">
-    <!-- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0//css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> -->
+    <link href='/fonts/Roboto.css' rel='stylesheet' type='text/css'>
 
     <!-- LIBARIES -->
     <link rel="stylesheet" href="/css/drooltip.css" media="screen" >
