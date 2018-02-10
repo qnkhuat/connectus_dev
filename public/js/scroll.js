15ec0713090b8feab33db844b21600e2fd731c3b
Ngoc <khuatquangngoc98@gmail.com>
2018-02-10 09:25:53 +0700

    change components

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function($) {
   $('.language.catigories').click(function() {
     distance = $('#courses').offset().top -20;
-    console.log(distance);
-    $('html, body').animate({scrollTop: distance},1000);
+    $('html, body').animate({scrollTop: distance},500);
   });
 });
