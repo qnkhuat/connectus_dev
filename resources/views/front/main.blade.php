110eee6f9bf952ba104aa8b8619e4edb65d30d42
Ngoc <khuatquangngoc98@gmail.com>
2018-03-06 15:21:21 +0700

    change word

--- resources/views/front/main.blade.php
+++ resources/views/front/main.blade.php
@@ -2,11 +2,13 @@
 
 @section('banner')
 <div id="banner">
+
   <div class="banner-slogan">
     <h1>CONNECTUS</h1>
     <h3>Học nhiều hơn - Tiết kiệm hơn</h3>
     <button type="button" class="search-button" name="button"><a href="/search">Tìm khoá học <i class="fa fa-search" aria-hidden="true"></i> </a></button>
   </div>
+
   <div class="category">
 
     <div class="language catigories">
