20bad4a88c710bb497dc117b0d04f2bf02ff1c42
Ngoc <khuatquangngoc98@gmail.com>
2018-04-09 23:39:35 +0700

    change some responsive

--- public/js/menu.js
+++ public/js/menu.js
@@ -5,32 +5,32 @@ jQuery(document).ready(function($) {
 
 
   //============mobile popup animation
-  var w = $(document).width();
-  if(w<=600){
-    $('.courses-item-box').click(function() {
-      $('#course-popup-mobile').css('display', 'block');
-      $('.wrapup-button-down').css('display', 'none');
-      $('.wrapup-button-up').css('display', 'inline-block');
-      setTimeout(function(){
-        $('#course-popup-mobile').css('transform', 'translate(0)');
-      },1);
-    });
-  }
+  // var w = $(document).width();
+  // if(w<=600){
+  //   $('.courses-item-box').click(function() {
+  //     $('#course-popup-mobile').css('display', 'block');
+  //     $('.wrapup-button-down').css('display', 'none');
+  //     $('.wrapup-button-up').css('display', 'inline-block');
+  //     setTimeout(function(){
+  //       $('#course-popup-mobile').css('transform', 'translate(0)');
+  //     },1);
+  //   });
+  // }
 
-
-    //wrapup for mobile course-popup
-    $('.wrapup-button').click(function() {
-      if($('#course-popup-mobile .course-popup-detail').css('display')!='none'){
-        $('#course-popup-mobile .course-popup-detail').css('display', 'none');
-        $('.wrapup-button-up').css('display', 'none');
-        $('.wrapup-button-down').css('display', 'inline-block');
-
-      }else{
-        $('#course-popup-mobile .course-popup-detail').css('display', 'block');
-        $('.wrapup-button-down').css('display', 'none');
-        $('.wrapup-button-up').css('display', 'inline-block');
-      }
-    });
+    //
+    // //wrapup for mobile course-popup
+    // $('.wrapup-button').click(function() {
+    //   if($('#course-popup-mobile .course-popup-detail').css('display')!='none'){
+    //     $('#course-popup-mobile .course-popup-detail').css('display', 'none');
+    //     $('.wrapup-button-up').css('display', 'none');
+    //     $('.wrapup-button-down').css('display', 'inline-block');
+    //
+    //   }else{
+    //     $('#course-popup-mobile .course-popup-detail').css('display', 'block');
+    //     $('.wrapup-button-down').css('display', 'none');
+    //     $('.wrapup-button-up').css('display', 'inline-block');
+    //   }
+    // });
 
 
 //change when click interest-butotn
