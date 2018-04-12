defca48d1650ddc04e6313fca1c2959bd2f36400
Ngoc <khuatquangngoc98@gmail.com>
2018-04-12 16:12:35 +0700

    add 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,14 @@
-trang khong ton tai
+@extends('front.layout')
+
+
+@section('banner')
+<img src="images/header/error.jpg" class="error" alt="">
+<div class="content">
+  <p>Sorry...<br>This page is sleeping!</p>
+</div>
+@endsection
+
+@section('latter_css')
+<link rel="stylesheet" href="/css/error.css" media="screen" >
+<link href="https://fonts.googleapis.com/css?family=Cabin+Sketch:700" rel="stylesheet">
+@endsection
